00b3b90bde4740c867edf33ef64dacd8a34606d3
OriChaiMatan <ori.chaimatan@gmail.com>
2024-08-22 17:37:07 +0300

    Added Count Down cmp

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div>Hey Vue
-    <ShowTime />
+    <CountDown :targetTime="Date.now() + 1000 * 60 * 0.5" />
   </div>
 </template>
 
 <script>
+import CountDown from './cmps/CountDown.vue';
 import ShowTime from "./cmps/ShowTime.vue";
 export default {
   components: {
     ShowTime,
+    CountDown,
   },
 };
 </script>
---
+++ src/cmps/CountDown.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="count-down" :style="countdownStyle">
+    <span>{{ formattedTime }}</span>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    targetTime: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      remainingTime: this.calculateRemainingTime()
+    };
+  },
+  computed: {
+    formattedTime() {
+      const minutes = Math.floor((this.remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((this.remainingTime % (1000 * 60)) / 1000);
+
+      return `${this.padTime(minutes)}:${this.padTime(seconds)}`;
+    },
+    countdownStyle() {
+      return {
+        color: this.remainingTime <= 10000 ? 'red' : 'black' // Changes to red in the last 10 seconds
+      };
+    }
+  },
+  created() {
+    this.startCountdown();
+  },
+  methods: {
+    calculateRemainingTime() {
+      return Math.max(this.targetTime - Date.now(), 0);
+    },
+    startCountdown() {
+      this.countdownInterval = setInterval(() => {
+        this.remainingTime = this.calculateRemainingTime();
+
+        if (this.remainingTime <= 0) {
+          clearInterval(this.countdownInterval);
+        }
+      }, 1000);
+    },
+    padTime(value) {
+      return String(value).padStart(2, '0');
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.countdownInterval);
+  }
+};
+</script>
+
+<style scoped>
+.count-down {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
